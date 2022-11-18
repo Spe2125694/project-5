93f175fc3dd388ff4f8343d861681d51bf556c35
Spe2125694 <spe2125694@maricopa.edu>
2022-11-17 20:30:52 -0700

    Completed 3 TODOs

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -1,5 +1,7 @@
 package com.example.sorting;
 
+import org.jetbrains.annotations.NotNull;
+
 public class SortingUtility {
 
     /**
@@ -22,10 +24,19 @@ public class SortingUtility {
      * @param <T>
      * @see <a href="https://en.wikipedia.org/wiki/Gnome_sort">https://en.wikipedia.org/wiki/Gnome_sort</a>
      */
-    public static <T extends Comparable<T>> void gnomeSort(T[] data) {
 
-        // TODO implement pseudocode above
+    public static <T extends Comparable<T>> void gnomeSort(T @NotNull [] data) {
 
+        int i = 0;
+        int n = data.length;
+        while(i < n) {
+            if(i == 0 || data[i - 1].compareTo(data[i]) <= 0) {
+                i++;
+            } else {
+                swap(data, i, i - 1);
+                i--;
+            }
+        }
     }
 
     /**
@@ -34,9 +45,9 @@ public class SortingUtility {
      * Implement an optimized gnome sort per the pseudocode below.
      * <p>
      * <pre>
-     *    method gnomierSort(a[])
-     *       for pos in 1 to length(a)
-     *       gnomierSort(a, pos)
+     *    method gnomierSort(data[])
+     *       for i in 1 to length(data)
+     *       gnomierSort(data, i)
      * </pre>
      *
      * @param data
@@ -45,7 +56,9 @@ public class SortingUtility {
      */
     public static <T extends Comparable<T>> void gnomierSort(T[] data) {
 
-        // TODO implement pseudocode above
+        for(int i = 1; i < data.length; i++){
+          gnomierSort(data, i);
+        }
 
     }
 
@@ -55,7 +68,7 @@ public class SortingUtility {
      * Implement an optimized gnome sort per the pseudocode below.
      * <p>
      * <pre>
-     *    method gnomierSort(a[], upperBound)
+     *    method gnomierSort(data[], upperBound)
      *       pos = upperBound
      *       while pos > 0 and a[pos-1] > a[pos]
      *          swap a[pos-1] and a[pos]
@@ -65,12 +78,16 @@ public class SortingUtility {
      * @param data
      * @param <T>
      */
-    private static <T extends Comparable<T>> void gnomierSort(T[] data, int upperBound) {
 
-        // TODO implement pseudocode above
+    private static <T extends Comparable<T>> void gnomierSort(T[] data, int upperBound) {
+        int i = upperBound;
+        while(i > 0 && data[i].compareTo(data[i - 1]) <= 0){
+            swap(data, i, i - 1);
+            i--;
+        }
     }
 
-    private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2) {
+    private static <T extends Comparable<T>> void swap(T @NotNull [] data, int index1, int index2) {
 
         T temp = data[index1];
         data[index1] = data[index2];
